dc55ac77b5a4fd6cdb21277ef335f49c0799c13b
fadyramsis <fadyramsis0@gmail.com>
2021-03-28 13:37:30 +0200

    JPA Relationships

--- src/main/java/guru/springframework/domain/Ingredient.java
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -3,6 +3,7 @@ package guru.springframework.domain;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import java.math.BigDecimal;
 
 public class Ingredient {
@@ -13,6 +14,7 @@ public class Ingredient {
     private String description;
     private BigDecimal amount;
 
+    @ManyToOne
     private Recipe recipe;
 
     public Long getId() {
