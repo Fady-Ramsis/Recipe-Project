cb36d4395220cd4e52a43289dac8987ad2e68fb7
fadyramsis <fadyramsis0@gmail.com>
2021-03-29 13:14:14 +0200

    Many To Many JPA Relationships

--- src/main/java/guru/springframework/Spring5RecipeAppApplication.java
+++ src/main/java/guru/springframework/Spring5RecipeAppApplication.java
@@ -2,11 +2,19 @@ package guru.springframework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import javax.servlet.annotation.WebServlet;
 
 @SpringBootApplication
 public class Spring5RecipeAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Spring5RecipeAppApplication.class, args);
+
 	}
+
+
 }
+
