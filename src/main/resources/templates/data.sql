e80fab15c5149587ea7488f82bff830e74eebcac
fadyramsis <fadyramsis0@gmail.com>
2021-04-01 11:58:03 +0200

    Database Initialization with Spring

---
+++ src/main/resources/templates/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO category (description) VALUE ('EGYPTIAN');
+INSERT INTO category (description) VALUE ('ITALIAN');
+INSERT INTO category (description) VALUE ('AMERICAN');
+INSERT INTO category (description) VALUE ('FAST FOOD');
+INSERT INTO unit_of_measure (description) VALUE ('CUP');
+INSERT INTO unit_of_measure (description) VALUE ('Tablespoon');
+INSERT INTO unit_of_measure (description) VALUE ('Pinch');
+INSERT INTO unit_of_measure (description) VALUE ('ounce');
