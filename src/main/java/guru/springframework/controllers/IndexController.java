16845f98dae06eaa1c27caaa74ba7667c6e1c56e
fadyramsis <fadyramsis0@gmail.com>
2021-04-01 18:30:28 +0200

    display list of Recipe

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,16 +1,42 @@
 package guru.springframework.controllers;
 
+import guru.springframework.domain.Category;
+import guru.springframework.domain.UnitOfMeasure;
+import guru.springframework.repositories.CategoryRepository;
+import guru.springframework.repositories.UnitOdMeasureRepository;
+import guru.springframework.services.RecipeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 /**
  * Created by jt on 6/1/17.
  */
 @Controller
 public class IndexController {
+    private final RecipeService recipeService;
+
+    public IndexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+   /* private CategoryRepository categoryRepository;
+    private UnitOdMeasureRepository unitOdMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOdMeasureRepository unitOdMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOdMeasureRepository = unitOdMeasureRepository;
+    }*/
+
 
     @RequestMapping({"", "/", "/index"})
-    public String getIndexPage(){
+    public String getIndexPage(Model model){
+     /*   Optional<Category>categoryOptional=categoryRepository.findbyDescription("EGYPTIAN");
+        Optional<UnitOfMeasure>unitOfMeasureOptional=unitOdMeasureRepository.findbyDescription("CUP");
+        System.out.println("Cat Id"+categoryOptional.get().getId());
+        System.out.println("Unit Id"+unitOfMeasureOptional.get().getId());*/
+        model.addAttribute("recipes", recipeService.getRecipes());
         return "index";
     }
 
--- src/main/java/guru/springframework/repositories/UnitOdMeasureRepository.java
+++ src/main/java/guru/springframework/repositories/UnitOdMeasureRepository.java
@@ -1,7 +1,11 @@
 package guru.springframework.repositories;
 
+import guru.springframework.domain.Category;
 import guru.springframework.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOdMeasureRepository extends CrudRepository<UnitOfMeasure,Long> {
+    Optional<UnitOfMeasure> findbyDescription(String description);
 }
